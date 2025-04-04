846fafd435620e93d2f3196efb83e5207764aa3b
Dillstan <75856962+Dillstan@users.noreply.github.com>
2025-04-04 16:23:07 +0000

    testing

--- 04_03_25/fakePriorityQueue.h
+++ 04_03_25/fakePriorityQueue.h
@@ -17,6 +17,7 @@ void fakePriorityQueue<t>::enqueue(const t &queueElement)
     newNode = new node<t>;
     newNode->data = new t(queueElement);
     newNode->link = nullptr;
+    
     if (this->isEmptyQueue())
     {
         this->queueFront = newNode;
@@ -24,6 +25,44 @@ void fakePriorityQueue<t>::enqueue(const t &queueElement)
     }
     else
     {
+        //start at the front
+        node<t>* current = this->queueFront;
+        node<t>* previous = nullptr;
+        while (current != nullptr)
+        {
+            // if new is higher priority than current, place before
+            if (newNode->data > current->data)
+            {
+                //if previous is null, then it is the new front
+                if(previous == nullptr)
+                    this->queueFront = newNode;
+                //else place between previous and current
+                else
+                    previous->link = newNode;
+                
+                //either way the new node will be before the current
+                newNode->link = current;
+                return;
+            }
+            //if current is the same as the new node put new after the current
+            else if(current->data == newNode->data)
+            {
+                newNode->link = current->link;
+                current->link = newNode;
+
+                if (current == this->queueRear)
+                {
+                    this->queueRear = newNode;
+                }
+                return;
+            }
+
+            //else, go to next node
+            previous = current;
+            current = current->link;
+        } 
+
+        //if it is not equal to any in list, set as rear
         this->queueRear->link = newNode;
         this->queueRear = newNode;
     }
